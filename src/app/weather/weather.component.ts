05b5d84c01efa2c2e87d6aa379dde94d6e337e8d
sunil-poudel <suneelpdl90@gmail.com>
2025-05-16 15:49:29 +0545

    add input box and button

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent {
 
+  protected readonly onsubmit = onsubmit;
+  city:string = '';
+
+  getWeather():void {
+    console.log("Getting weather for "+this.city);
+  }
 }
